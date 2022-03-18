2e59a16ffae06b1f2fdad3b0283e5d54badb1f89
dulanjanabandara <dulanjanauop@gmail.com>
2022-03-18 17:37:28 +0530

    Index file was modified to generate the templates

--- index.js
+++ index.js
@@ -38,21 +38,47 @@ const url = require("url");
 // 1. Create the server
 // 2. Start the server
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the Overview.");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
+    res.end(tempOverview);
+
+    // Product Page
   } else if (pathName === "/product") {
     res.end("This is the Product.");
+
+    // API
   } else if (pathName === "/api") {
-    res.writeHead(200, {
-      "Content-type": "application/json",
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    // Not Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
