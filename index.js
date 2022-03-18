4ea25663ae1217b4e50328be76dae1139dfe6f29
dulanjanabandara <dulanjanauop@gmail.com>
2022-03-18 06:33:38 +0530

    Created a simple api in pure nodejs.

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+////////////////////////////////////////////////////////
+// FILE READING
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) {
+//     return console.log("ERROR!");
+//   }
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written!");
+//       });
+//     });
+//   });
+// });
+
+// console.log("Will read file!");
+
+////////////////////////////////////////////////////////
+// CREATING A WEB SERVER
+//
+// 1. Create the server
+// 2. Start the server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview.");
+  } else if (pathName === "/product") {
+    res.end("This is the Product.");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+const PORT = 8000;
+
+server.listen(PORT, "127.0.0.1", () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
